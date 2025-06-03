6cf2dcc9314badba595400f81c7429cc2b6df5f1
GabrielGiannini <gabriel_giannini@yahoo.com.ar>
2025-06-03 16:10:51 -0300

    Encabezado API

---
+++ API.h
@@ -0,0 +1,9 @@
+#ifndef API_H_INCLUDED
+#define API_H_INCLUDED
+
+void verRanking();
+size_t WriteCallback(void *contents, size_t , size_t nmemb, void *userp);
+void guardarRanking(const char *codigoGrupo, const char *nombreJugador, int esGanador);
+void resetearGrupo(const char *codigoGrupo);
+
+#endif // API_H_INCLUDED
