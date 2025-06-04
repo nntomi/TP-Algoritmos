f68459236c45724e659665997cef0a6e28a8fa8b
GabrielGiannini <gabriel_giannini@yahoo.com.ar>
2025-06-03 21:38:25 -0300

    Update Pila.c

--- Pila.c
+++ Pila.c
@@ -15,22 +15,22 @@ int pilaLlena(const tPila *p, unsigned cantBytes)
 
     return aux == NULL || info == NULL;
 
-     //return 1; puede estar o no disponible no es necesario hacer lo anterior. Digo que esta disponible.
 }
 
 int apilar(tPila *p,const void *d, unsigned cantBytes)
-{   tNodo *nue;
+{
+    tNodo *nue;
     if((nue = (tNodo *)malloc(sizeof(tNodo)))== NULL ||
-       (nue->info = malloc (cantBytes))== NULL)
-    { //Hacer la reserva de memoria afuera del if y luego comparar.
+            (nue->info = malloc (cantBytes))== NULL)
+    {
         free(nue);
-        return 0;
+        return ERROR;
     }
     memcpy(nue->info,d,cantBytes);
     nue->tamInfo = cantBytes;
     nue->sig = *p;
     *p = nue;
-    return 1;
+    return OK;
 
 }
 
@@ -38,10 +38,10 @@ int verTope (const tPila *p,void *d, unsigned cantBytes)
 {
     if(*p==NULL)
     {
-        return 0;
+        return PILA_VACIA;
     }
     memcpy(d,(*p)->info,minimo(cantBytes,(*p)->tamInfo));
-    return 1;
+    return OK;
 }
 
 int pilaVacia(const tPila *p)
@@ -55,14 +55,14 @@ int desapilar(tPila *p,void *d,unsigned cantBytes)
 
     if(aux == NULL)
     {
-        return 0;
+        return PILA_VACIA;
     }
 
     *p = aux->sig;
     memcpy(d,aux->info,minimo(cantBytes,aux ->tamInfo));
     free(aux->info);
     free(aux);
-    return 1;
+    return OK;
 }
 
 void vaciarPila(tPila *p)
@@ -75,6 +75,6 @@ void vaciarPila(tPila *p)
         free(aux->info);
         free(aux);
 
-
     }
 }
+
